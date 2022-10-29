364940fe1bf6444fd44485415aec3ca27dff74a1
akshaykhoje <khojeakshay02@gmail.com>
2022-10-29 15:09:02 +0530

    completed assgt 5

--- 5. IP Fragmentation/ip_fragmentation.py
+++ 5. IP Fragmentation/ip_fragmentation.py
@@ -33,15 +33,34 @@ class IPFragmentationCalculator:
     print(f"The total size is : {tl}")    
     
   
-  # def MF_Flag():
-  #   pass
+  def MF_Flag(self):
+    '''records the MF flag'''
+    sr_pkt_no = self.frags                   # serial packet number
+    i = 0
+    
+    while( True ):
+      i += 1
+      if (sr_pkt_no > 1):
+        sr_pkt_no = sr_pkt_no - 1
+        print(f"MF {i}: 1")
+      else:
+        print(f"MF {i}: 0")
+        break
+    
 
   def Offset(self):
-    scaling_factor = 8
-    tl = 0
-    while tl < self.pkt_size:
-      tl = tl + self.mtu
-    pass
+    scaling_factor = 8  
+    payload = ceil((self.pkt_size - 20) / scaling_factor)      # actual data to be sent in bytes
+    fragment_payload = ceil((self.mtu - 20) / scaling_factor)      # data bytes that each fragment carries in bytes
+    offset = 0                  
+    i = 0                                   # loop counter
+    
+    while(i < self.frags):
+      i += 1
+      print(f"Offset {i} : {offset}")
+      offset = fragment_payload*i
+      payload = payload - fragment_payload       
+  
   
 if __name__ == "__main__":
   PKT_SIZE = int(input("Enter the size of IP data packet to be sent : "))
@@ -49,11 +68,13 @@ if __name__ == "__main__":
   
   if (isinstance(PKT_SIZE, int) and isinstance(MTU, int)):
     obj1 = IPFragmentationCalculator(PKT_SIZE, MTU)
+    print()
     obj1.no_of_fragments()
+    print()
+    obj1.MF_Flag()
+    print()
+    obj1.Offset()
     
-    
-# obj1 = IPFragmentationCalculator()
-# obj1.no_of_fragments()
 
   
   
---
+++ 6. TCP congestion/RESOURCES.md
@@ -0,0 +1,5 @@
+# Resources
+
+- https://www.stackpath.com/edge-academy/what-is-cwnd-and-rwnd/
+- https://www.geeksforgeeks.org/tcp-congestion-control/
+- https://www.geeksforgeeks.org/tcp-tahoe-and-tcp-reno/
